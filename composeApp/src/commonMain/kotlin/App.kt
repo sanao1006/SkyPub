d02123f9c64acf4e08bc05f185c4a42715470a44
sanao1006 <naosak1006@gmail.com>
2024-08-05 23:13:00 +0900

    fix initialize

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,9 +1,11 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
+import app.skypub.data.repository.InitializeRepository
 import app.skypub.feature.auth.AuthScreenNavigation
 import app.skypub.home.HomeScreen
 import cafe.adriel.voyager.navigator.Navigator
@@ -16,10 +18,13 @@ import org.koin.compose.koinInject
 @Composable
 fun App() {
     Napier.base(DebugAntilog())
+    val initializeRepository = koinInject<InitializeRepository>()
+    LaunchedEffect(Unit) {
+        initializeRepository.initializeToken()
+    }
     val dataStore: DataStore<Preferences> = koinInject<DataStore<Preferences>>()
     val tokenPair = dataStore.data.map {
         it[stringPreferencesKey("access_jwt")] to it[stringPreferencesKey("refresh_jwt")]
-
     }.collectAsState(initial = "empty_access_jwt_token" to "empty_refresh_jwt_token").value
     val isAlreadyLogin =
         tokenPair.first != "empty_access_jwt_token" && tokenPair.second != "empty_refresh_jwt_token"
