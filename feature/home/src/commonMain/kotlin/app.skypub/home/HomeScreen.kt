55bb7266fc8b3f7ffbfc79da54df7ddf1b7f8e20
sanao1006 <naosak1006@gmail.com>
2024-08-09 19:10:19 +0900

    add navigation bar

--- feature/home/src/commonMain/kotlin/app.skypub/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/app.skypub/home/HomeScreen.kt
@@ -3,10 +3,21 @@ package app.skypub.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import cafe.adriel.voyager.core.screen.Screen
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
@@ -17,7 +28,22 @@ class HomeScreen : Screen {
     @Composable
     override fun Content() {
         val viewmodel: HomeViewModel = koinInject<HomeViewModel>()
-        Scaffold {
+        var selectedItem by remember { mutableIntStateOf(0) }
+
+        Scaffold(
+            bottomBar = {
+                NavigationBar {
+                    NavigationBarMenu.entries.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            icon = { Icon(imageVector = item.icon, contentDescription = "") },
+                            label = { Text(item.label) },
+                            selected = selectedItem == index,
+                            onClick = { selectedItem = index }
+                        )
+                    }
+                }
+            }
+        ) {
             Column {
                 Text(text = "Home")
                 val feeds = viewmodel.feed.collectAsState()
@@ -32,4 +58,10 @@ class HomeScreen : Screen {
             }
         }
     }
+}
+
+enum class NavigationBarMenu(val label: String, val icon: ImageVector) {
+    Home("Home", Icons.Filled.Home),
+    Notifications("Notifications", Icons.Filled.Notifications),
+//    Messages("Messages", Icons.AutoMirrored.Filled.Message)
 }
