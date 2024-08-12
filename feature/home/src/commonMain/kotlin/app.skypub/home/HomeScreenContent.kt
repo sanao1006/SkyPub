42630e6ec32444ab90532cc3f531070c147a5265
sanao1006 <naosak1006@gmail.com>
2024-08-12 16:04:23 +0900

    create timeline post icons

--- feature/home/src/commonMain/kotlin/app.skypub/home/HomeScreenContent.kt
+++ feature/home/src/commonMain/kotlin/app.skypub/home/HomeScreenContent.kt
@@ -1,26 +1,38 @@
 package app.skypub.home
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.Repeat
+import androidx.compose.material.icons.sharp.FavoriteBorder
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.skypub.network.model.FeedItem
 import com.github.panpf.sketch.AsyncImage
 import com.github.panpf.sketch.request.ComposableImageRequest
@@ -50,7 +62,7 @@ fun HomeScreenContent(
                     HomeScreenPostItem(
                         feed = feed,
                         modifier = Modifier
-                            .padding(vertical = 12.dp)
+                            .padding(top = 12.dp)
                     )
                 }
             }
@@ -71,7 +83,6 @@ fun HomeScreenPostItem(
         )
         Spacer(modifier = Modifier.size(8.dp))
         Column(modifier = Modifier.weight(1f)) {
-
             Text(text = feed.post.author.displayName)
             Spacer(modifier = Modifier.width(4.dp))
             Text(
@@ -83,6 +94,59 @@ fun HomeScreenPostItem(
                 text = feed.post.record.jsonObject["text"]?.jsonPrimitive?.content ?: "",
                 style = MaterialTheme.typography.bodyMedium
             )
+            Spacer(modifier = Modifier.height(6.dp))
+            PostContentIcons(feed = feed)
+        }
+    }
+}
+
+@Composable
+fun PostContentIcons(
+    feed: FeedItem,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.fillMaxWidth(0.7f)
+    ) {
+        ContentIcons.entries.forEach { item ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.wrapContentSize()
+            ) {
+                Icon(
+                    modifier = Modifier.scale(0.7f).clickable { },
+                    imageVector = item.icon,
+                    contentDescription = ""
+                )
+                var count: String = item.getIconCount(feed, item).let {
+                    if (it == "0") ""
+                    else it
+                }
+                Text(
+                    text = count,
+                    modifier = Modifier.align(Alignment.Top),
+                    fontSize = 14.sp
+                )
+            }
+        }
+    }
+}
+
+enum class ContentIcons(val icon: ImageVector) {
+    ChatBubbleOutline(Icons.Default.ChatBubbleOutline),
+    Repeat(Icons.Default.Repeat),
+    FavoriteBorder(Icons.Sharp.FavoriteBorder)
+    ;
+
+    fun getIconCount(feed: FeedItem, contentIcons: ContentIcons): String {
+        return when (contentIcons) {
+            ChatBubbleOutline -> feed.reply?.root?.jsonObject?.get("replyCount")?.jsonPrimitive?.content
+                ?: ""
+
+            Repeat -> feed.reply?.root?.jsonObject?.get("repostCount")?.jsonPrimitive?.content ?: ""
+            FavoriteBorder -> feed.reply?.root?.jsonObject?.get("likeCount")?.jsonPrimitive?.content
+                ?: ""
         }
     }
 }
