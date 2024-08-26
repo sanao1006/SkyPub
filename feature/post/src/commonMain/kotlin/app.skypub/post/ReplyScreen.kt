f89a77248463fa444ae0aff516dee1f46010c850
sanao1006 <naosak1006@gmail.com>
2024-08-26 22:30:58 +0900

    Show alert dialog when leaving reply screen

--- feature/post/src/commonMain/kotlin/app.skypub/post/ReplyScreen.kt
+++ feature/post/src/commonMain/kotlin/app.skypub/post/ReplyScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,6 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,12 +43,37 @@ class ReplyScreen(
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
+        var isDialogShow by rememberSaveable { mutableStateOf(false) }
+
+        if (isDialogShow) {
+            AlertDialog(
+                onDismissRequest = { isDialogShow = false },
+                title = { Text("Save Draft") },
+                text = { Text("Do you want to save this post as a draft?") },
+                confirmButton = {
+                    TextButton(onClick = {
+                        isDialogShow = false
+                        navigator.pop()
+                    }) {
+                        Text("Save")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        isDialogShow = false
+                        navigator.pop()
+                    }) {
+                        Text("Discard")
+                    }
+                }
+            )
+        }
         Scaffold(
             topBar = {
                 TopAppBar(
                     title = {},
                     navigationIcon = {
-                        IconButton(onClick = { navigator.pop() }) {
+                        IconButton(onClick = { isDialogShow = true }) {
                             Icon(Icons.Default.Close, contentDescription = "")
                         }
                     },
@@ -73,7 +103,7 @@ fun ReplyPost(
     modifier: Modifier = Modifier,
     name: String,
     thumbnail: String,
-    post: String
+    post: String,
 ) {
     Row(
         modifier = modifier,
