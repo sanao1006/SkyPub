d81359460167336c1126fbc726e534c57e65afa8
sanao1006 <naosak1006@gmail.com>
2024-09-01 22:32:58 +0900

    init post detail screen

---
+++ feature/postdetail/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("app.skypub.convention.kotlinMultiplatform")
+    id("app.skypub.convention.composeMultiplatform")
+}
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.voyager)
+            implementation(projects.core.data)
+            api(projects.core.network)
+        }
+    }
+}
---
+++ feature/postdetail/src/commonMain/kotlin/com.example/postdetail/PostDetailScreen.kt
@@ -0,0 +1,11 @@
+package com.example.postdetail
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class PostDetailScreen : Screen {
+    @Composable
+    override fun Content() {
+        TODO("Not yet implemented")
+    }
+}
