bea7b70ce206606e3a71470deca090f5fcd7db01
sanao1006 <naosak1006@gmail.com>
2024-09-01 22:41:51 +0900

    init post detail screen appbar

--- feature/postdetail/src/commonMain/kotlin/com.example/postdetail/PostDetailScreen.kt
+++ feature/postdetail/src/commonMain/kotlin/com.example/postdetail/PostDetailScreen.kt
@@ -1,11 +1,37 @@
 package com.example.postdetail
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 
 class PostDetailScreen : Screen {
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
-        TODO("Not yet implemented")
+        val navigator = LocalNavigator.currentOrThrow
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Thread") },
+                    navigationIcon = {
+                        IconButton(onClick = { navigator.pop() }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "Back"
+                            )
+                        }
+                    }
+                )
+            }
+        ) { }
     }
 }
