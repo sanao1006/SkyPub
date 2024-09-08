fd3a813f3d20a58193c842354faf1cb4997bc2d3
sanao1006 <naosak1006@gmail.com>
2024-09-08 23:23:06 +0900

    fix

--- feature/composables/src/commonMain/kotlin/app.skypub/composables/RepostBottomSheet.kt
+++ feature/composables/src/commonMain/kotlin/app.skypub/composables/RepostBottomSheet.kt
@@ -17,22 +17,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-
-
-@Composable
-fun a() {
-    var openModal by rememberSaveable { mutableStateOf(false) }
-    if (openModal) RepostBottomSheet(onDismiss = { openModal = it })
-}
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
